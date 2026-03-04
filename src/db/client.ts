cfcaf2144e1b6cc1254968f3ab1d6df4e4f11a63
Muhammad Faris Hadi Mulyo <kimfais1234@gmail.com>
2026-03-04 23:35:04 +0700

    refactor(client): remove db tranasaction adapter

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,4 +1,3 @@
-import { TransactionalAdapterDrizzleOrm } from '@nestjs-cls/transactional-adapter-drizzle-orm';
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
@@ -6,7 +5,5 @@ import * as schema from './schema';
 const db = drizzle(process.env.DB_URL!, { schema });
 
 export type DB = typeof db;
-
-export type DbTransactionAdapter = TransactionalAdapterDrizzleOrm<typeof db>;
 
 export default db;
